e9492713016dba6edc2bc3f36ca5a9d03ae600d5
Jean Parpaillon <jean.parpaillon@free.fr>
2014-10-01 14:58:25 +0200

    Allow to set external authentication cookie from application env

--- src/auth/dbus_auth_external.erl
+++ src/auth/dbus_auth_external.erl
@@ -14,8 +14,25 @@
 -export([init/0,
 	 challenge/2]).
 
+-define(cookie, <<"31303030">>).
+
 init() ->
-    {ok, <<"AUTH EXTERNAL 31303030\r\n">>}.
+    Cookie = get_cookie(),
+    {ok, <<"AUTH EXTERNAL ", Cookie/binary, "\r\n">>}.
 
 challenge(_, _) ->
     {error, invalid_challenge}.
+
+%%%
+%%% Priv
+%%%
+get_cookie() ->
+    case application:get_env(dbus, external_cookie) of
+	undefined -> ?cookie;
+	{ok, Val} when is_list(Val) ->
+	    list_to_binary(Val);
+	{ok, Val} when is_binary(Val) ->
+	    Val;
+	{ok, Val} when is_integer(Val) ->
+	    integer_to_binary(Val)
+    end.
